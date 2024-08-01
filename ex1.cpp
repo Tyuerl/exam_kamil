a900a447356a216fbe6df20a341008c6bea30b73
tyuerls <gresso507@gmail.com>
2024-08-02 00:09:08 +0300

    asdf

---
+++ ex1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	int* array;
+	cin >> n;;
+	array = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> array[i];
+	}
+	for (int i = 1; i < n; i++)
+		for (int j = i; j > 0 && array[j - 1] > array[j]; j--)
+			swap(array[j - 1], array[j]);
+	for (int i = 0; i < n; i++)
+		cout << array[i] << " ";
+		return 0;
+}
+
+
